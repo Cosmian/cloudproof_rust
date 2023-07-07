c158fdb28e78ed2438080a0629cbf57fae4bf3fa
Manuthor <manu.coste@gmail.com>
2023-07-07 15:17:52 +0200

    feat: merge all new crates to cloudproof crate

--- crates/cloudproof/src/lib.rs
+++ crates/cloudproof/src/lib.rs
@@ -1,9 +1,13 @@
 //! Meta crate in order to merge other crates
 
 #[cfg(feature = "ffi")]
+pub use cloudproof_aesgcm::ffi as aesgcm_ffi;
+#[cfg(feature = "ffi")]
 pub use cloudproof_anonymization::ffi as anonymization_ffi;
 #[cfg(feature = "ffi")]
 pub use cloudproof_cover_crypt::ffi as cover_crypt_ffi;
+#[cfg(feature = "ffi")]
+pub use cloudproof_ecies::ffi as ecies_ffi;
 #[cfg(feature = "ffi")]
 pub use cloudproof_findex::ffi as findex_ffi;
 #[cfg(feature = "ffi")]
